8f128e3255557c0f5c7de3033e1997c9168453da
Glitch (truthful-soprano) <none>
2019-10-24 06:25:40 +0000

    ⛪️🐛 Updated with Glitch

--- app/components/FormLib/Input.jsx
+++ app/components/FormLib/Input.jsx
@@ -1,13 +1,17 @@
 const React = require('react');
 require('../../styles.css');
 
-const Input = ({ label, name, type, value, onChange }) => {
+const Input = ({ label, name, type, value, error, onChange }) => {
   return (
     <div className="form-input-row">
       <label>
         {label}: 
       </label>
       <input type={type || 'text'} name={name} value={value} onChange={onChange} />
+      {
+        error &&
+        <span className="error">{error}</span>
+      }
     </div>
   );
 };
